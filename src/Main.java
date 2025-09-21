a2ae6e622d3bab47994f8453b6b8fb792bb24e78
Pugsy <nimsarakarunaratne@gmail.com>
2025-09-21 19:13:55 +0530

    Java Multiple Threads

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+class A extends Thread{
+    public void run(){
+        for (int i =0;  i<10; i++){
+            System.out.println("hi ");
+        }
+    }
+}
+
+class B extends Thread{
+    public void run(){
+        for (int i = 0; i<10; i++){
+            System.out.println("Hello ");
+        }
+    }
+}
+
+
+public class Main{
+    public static void main(String[] args){
+        A obj1 = new A();
+        B obj2 = new B();
+
+        obj1.start();
+        obj2.start();
+    }
+}

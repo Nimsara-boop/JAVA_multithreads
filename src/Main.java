306ef02df1cfb49e77a368f6d9faea79130b46fe
Pugsy <nimsarakarunaratne@gmail.com>
2025-09-22 23:26:41 +0530

    Priority setting with Threads

--- src/Main.java
+++ src/Main.java
@@ -20,6 +20,12 @@ public class Main{
         A obj1 = new A();
         B obj2 = new B();
 
+        System.out.println(obj1.getPriority());//priority range is 1-10. 1 is lowest, 10 is highest priority
+
+        B obj3 = new B();
+        obj3.setPriority(2); //this is how we set priorities
+        obj3.setPriority(Thread.NORM_PRIORITY); //can set priorities like this too
+
         obj1.start();
         obj2.start();
     }
